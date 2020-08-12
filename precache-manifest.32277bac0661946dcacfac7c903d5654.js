ca094230f4df445d903a721c99432f6ef29fc159
jinho6225 <jinho6225@hotmail.com>
2020-08-12 16:33:40 -0700

    Updates

---
+++ precache-manifest.32277bac0661946dcacfac7c903d5654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3999cd8c42f0292b20b6dc8158e566",
+    "url": "/todofam/index.html"
+  },
+  {
+    "revision": "60f7274729937e617a1c",
+    "url": "/todofam/static/css/main.03d107ed.chunk.css"
+  },
+  {
+    "revision": "eb9555e30bc79450f875",
+    "url": "/todofam/static/js/2.2e125172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todofam/static/js/2.2e125172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f7274729937e617a1c",
+    "url": "/todofam/static/js/main.4ca47905.chunk.js"
+  },
+  {
+    "revision": "54beff4274065216375a",
+    "url": "/todofam/static/js/runtime-main.bf9e0906.js"
+  }
+]);
